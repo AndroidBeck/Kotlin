93fd83d2706e4a0c4d29dd9d4427f9406470ac2d
Aleksandr Evdokimov <alexandr8@gmail.com>
2018-06-05 13:24:02 +0300

    FishFood fun()

--- src/AMS.kt
+++ src/AMS.kt
@@ -4,24 +4,35 @@ import java.util.*
 fun main(args: Array<String>) {
 
     println("${if (args[0].toInt() < 12) "Good Morning Kotlin" else "Good Night Kotlin"}")
+    feedTheFish()
+}
 
-//    print("Yello, ${args[0]}")
-//    dayOfWeek()
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and we'll fishes with: $food")
+}
+
+fun fishFood(day: String): String {
+    return when(day) {
+        "Sunday" -> "pellets"
+        "Monday" -> "small fish"
+        "Wednesday" -> "crabs"
+        "Friday" -> "weed"
+        "Saturday" -> "plankton"
+        else -> "Uncknown food"
+    }
+}
+
+fun randomDay(): String {
+    val week = listOf("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    return week[Random().nextInt(7)]
 }
 
 fun dayOfWeek() {
     println("What day is it today?")
     var cal = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
     var strDay: String = ""
-    when(cal) {
-        1 -> strDay = "Sunday"
-        2 -> strDay = "Monday"
-        3 -> strDay = "Tuesday"
-        4 -> strDay = "Wensday"
-        5 -> strDay = "Thursday"
-        6 -> strDay = "Friday"
-        7 -> strDay = "Saurday"
-        else -> strDay = "Uncknown day"
-    }
+
     println("It's $strDay !")
 }
