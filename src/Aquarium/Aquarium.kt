a3447c17da53c83167475ae14f98fee6244b799d
Aleksandr Evdokimov <alexandr8@gmail.com>
2018-06-08 00:01:41 +0300

    try some classes - add 2 files aquarium.kt and main.kt. Both are in package "Aquarium"

---
+++ src/Aquarium/Aquarium.kt
@@ -0,0 +1,7 @@
+package Aquarium
+
+class Aquarium {
+    val widt: Int = 20
+    val length: Int = 100
+    val height: Int = 40
+}
---
+++ src/Aquarium/main.kt
@@ -0,0 +1,9 @@
+package Aquarium
+
+fun main(args: Array<String>) {
+    buildAquarium()
+}
+
+fun buildAquarium() {
+    val myAquarium = Aquarium()
+}
