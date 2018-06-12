19290653eab0bbf39b5e15cc6e0c55ad4fdb435d
Aleksandr Evdokimov <alexandr8@gmail.com>
2018-06-13 01:14:50 +0300

    changed aquarium.kt and main.kt. Both are in package "Aquarium"

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -1,7 +1,11 @@
 package Aquarium
 
 class Aquarium {
-    val widt: Int = 20
-    val length: Int = 100
-    val height: Int = 40
+    var width: Int = 20
+    var length: Int = 100
+    var height: Int = 40
+
+    var volume: Int
+        get() = width * length * height / 1000
+        set(value) {value * 1000 / (width * length)}
 }
--- src/Aquarium/main.kt
+++ src/Aquarium/main.kt
@@ -6,4 +6,13 @@ fun main(args: Array<String>) {
 
 fun buildAquarium() {
     val myAquarium = Aquarium()
+    //Kotlin will call getter method even if it looks like we're calling gor the property directly
+    println("width = ${myAquarium.width} \n" +
+            "lenfth = ${myAquarium.length} \n" +
+            "height = ${myAquarium.height}")
+
+    myAquarium.height = 80
+    println("new height = ${myAquarium.height}")
+    println("volume = ${myAquarium.volume}")
+
 }
