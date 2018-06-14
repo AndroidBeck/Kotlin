5e171a6f07482bc2fc916c3e8d05d273bccd0e8a
Aleksandr Evdokimov <alexandr8@gmail.com>
2018-06-15 01:17:54 +0300

    Change fish //Testing constructors

--- src/Aquarium/Fish.kt
+++ src/Aquarium/Fish.kt
@@ -1,7 +1,34 @@
 package Aquarium
 
-    class Fish(val friendly: Boolean = true, volumeNeeded: Int) {
+class Fish(val friendly: Boolean = true, volumeNeeded: Int) {
 
-        //val size: Int
+    val size: Int
 
+    init {
+        println("First init block")
     }
+
+    constructor() : this(true, 9) {
+        println("Running secondary constructor")
+    }
+
+    init {
+        if (friendly) {
+            size = volumeNeeded
+        } else {
+            size = volumeNeeded * 2
+        }
+    }
+
+    init {
+        println("3rd init block.. $volumeNeeded.. ${this.size}")
+    }
+
+}
+
+fun makeDefaultFish() = Fish(true, 50)
+
+fun fishExample() {
+    val fish = Fish(true, 20)
+    println("Is fish friendly? ${fish.friendly}. Volume needed = ${fish.size}")
+}
