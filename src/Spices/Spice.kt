aef3ddf29f89172962b6aa0b62abd720d25985f0
Aleksandr Evdokimov <alexandr8@gmail.com>
2018-06-19 17:33:38 +0300

    Use learned stuff about classes

--- src/Spices/Spice.kt
+++ src/Spices/Spice.kt
@@ -7,7 +7,8 @@ fun main(args: Array<String>) {
 
 }
 
-abstract class Spice (val name: String, val spiciness: String = "mild", color: SpiceColor):
+//Making Spice a sealed class helps keep all the spices together in one file.
+abstract sealed class Spice (val name: String, val spiciness: String = "mild", color: SpiceColor):
         SpiceColor by color {
 
     override fun toString(): String = this.name
@@ -20,12 +21,19 @@ interface Grinder {
     fun grind()
 }
 
+enum class Color(val rgb: Int) {
+    RED(0xFF0000),
+    GREEN(0x00FF00),
+    BLUE(0x0000FF),
+    YELLOW(0xFFFF00)
+}
+
 interface SpiceColor {
-    val color: String
+    val color: Color
 }
 
 object YellowSpiceColor: SpiceColor {
-    override val color = "Yellow"
+    override val color = Color.YELLOW
 }
 
 class Curry(spiciness: String, name: String = "Curry", color: SpiceColor = YellowSpiceColor):
@@ -38,5 +46,4 @@ class Curry(spiciness: String, name: String = "Curry", color: SpiceColor = Yello
     override fun grind() {
         println("Grinding")
     }
-
 }
