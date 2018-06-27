805804ed974303726967a604589d27571e086ece
Aleksandr Evdokimov <alexandr8@gmail.com>
2018-06-27 09:49:44 +0300

    Change Book class (and eBook) using Pairs and Triples

--- src/Book.kt
+++ src/Book.kt
@@ -1,17 +1,34 @@
-open class Book(val title: String, val author:String) {
+open class Book(val title: String, val author:String, val year: Int) {
 
     private var currentPage: Int = 1
 
     open fun readPage() {
         currentPage++
     }
+
+    fun getTitleAndAuthor(): Pair<String, String> {
+        return title to author
+    }
+
+    fun getTitleAuthorAndYear(): Triple<String, String, Int> {
+        return Triple( title, author, year)
+    }
 }
 
-class eBook(val format: String = "text"): Book("","") {
+class eBook(val format: String = "text"): Book("","", 0) {
 
     var wordsRead = 0
 
     override fun readPage() {
         wordsRead = wordsRead + 250
     }
+}
+
+fun main(args: Array<String>) {
+    val book = Book("Sobaka Baskerviley", "A.Conan Doyle", 1895)
+    println(book.getTitleAndAuthor())
+    val titleAuthorYear = book.getTitleAuthorAndYear()
+    println(titleAuthorYear)
+    println("Here is your book ${titleAuthorYear.first} written by ${titleAuthorYear.second} " +
+            "in ${titleAuthorYear.third}.")
 }
