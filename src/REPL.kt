5b519d5a7a06ad99043682436d6d08e7755e813d
Aleksandr Evdokimov <alexandr8@gmail.com>
2018-08-30 00:28:16 +0300

    change REPL (recap lambdas)

--- src/REPL.kt
+++ src/REPL.kt
@@ -61,6 +61,14 @@ fun main(args: Array<String>) {
 //    println(fishnet.toString())
 //    println(fishnet.toList())
 
+//    Lesson 6 - lambdas recap
+    {println("Hello fish ")}()
+    val waterFilter = {dirty: Int -> dirty/2}
+    println(waterFilter(30))
+
+    data class Fish (val name: String)
+    val myFish = listOf(Fish("Flipper"), Fish("Moby Dick"), Fish("Dory"))
+    println(myFish.filter { it.name.contains("i") }.joinToString(", ") { it.name })
 
 }
 
